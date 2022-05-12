a98831b24cc2b91367dc7ab41bbda7fe19de194e
Cyril MELTZ <cyril.meltz@campdebases.com>
2022-05-12 15:31:11 +0200

    With flatten

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-toast',
-      version='0.1.2',
+      version='0.2.2',
       description='Singer.io tap for extracting data from the Client API',
       author='@lambtron',
       url='https://andyjiang.com',
